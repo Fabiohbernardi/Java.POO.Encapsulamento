c153e70f77ae16e2f9223b5eb54fc00441553bb0
fabio <fbernardi32@gmail.com>
2023-06-27 13:57:22 -0300

    JAVA - Encapsulamento

---
+++ src/Encapsulamento/Encapsulamento.java
@@ -0,0 +1,13 @@
+package Encapsulamento;
+
+public class Encapsulamento {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.abrirMenu();
+
+
+
+    }
+
+}
